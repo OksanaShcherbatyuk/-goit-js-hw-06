c7a82d53fc5369f328a249a4162af4812e52bafb
OksanaShcherbatyuk <anbony@gmail.com>
2022-10-11 13:46:36 +0300

    виконує 1 та 2 таскі

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const refs = {
+  list: document.querySelector("#ingredients"),
+}
+
+const items = ingredients.map(el => {
+  const elements = document.createElement("li");
+  elements.textContent = el;
+  elements.classList.add("item");
+  return elements;
+})
+
+refs.list.append(...items);
